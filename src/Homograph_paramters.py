0b941f6bce11eeaa456850f763b56c55a74eebe4
yasserAMuhammed <yasser.csd@gmail.com>
2014-05-16 20:49:06 +0200

    Add Merge feature and access bug fix

--- src/Homograph_paramters.py
+++ src/Homograph_paramters.py
@@ -72,15 +72,15 @@ def calcH(pts):
 	#solve equation
 	h = np.linalg.lstsq(A, b)[0];
 
-	for i in range(0,n):
-		iniPoint = np.array([[getStartX(pts, i)], [getStartY(pts, i)], [1]]);
-		print(' point ' + str(i) + '\n\t'+ str(np.array([[getEndX(pts, i)], [getEndY(pts, i)], [1]])) + '\n\t' + str(transform(iniPoint, h)));
+#	for i in range(0,n):
+#		iniPoint = np.array([[getStartX(pts, i)], [getStartY(pts, i)], [1]]);
+#		print(' point ' + str(i) + '\n\t'+ str(np.array([[getEndX(pts, i)], [getEndY(pts, i)], [1]])) + '\n\t' + str(transform(iniPoint, h)));
 #	print('shapes for A, h, b, reconstructed ' + str(A.shape) + str(h.shape) + str(b.shape) + str(reconstructedB.shape));
 	return h;
 
 #========================== NEW WRAP IMAGE
 
-def wrapImage(sourceImage , h):
+def wrapAndMergeImage(sourceImage , h, refImage):
 	# here we return new sourceImage with (2*width,2*height), and transform our image into destination .. with interpolating
 	height = sourceImage.shape[0];
 	width = sourceImage.shape[1];
@@ -90,14 +90,15 @@ def wrapImage(sourceImage , h):
 	max_mapped_i = max_mapped_j = int(-100000);
 
 	# calculate bounds of new image
+	print('Image A size ' + str(sourceImage.shape));
 
 	for i in range(0,height):
 		for j in range(0, width):
 			# perform multiplication	TODO add width,heigh shift
-			mappedPos = transform(np.array([[i],[j],[1]]), h);
+			mappedPos = transform(np.array([[j],[i],[1]]), h);
 			# let's
-			mapped_i = int(mappedPos[0][0])
-			mapped_j = int(mappedPos[1][0])
+			mapped_j = int(mappedPos[0][0])
+			mapped_i = int(mappedPos[1][0])
 			# update bounding box!
 			if mapped_i < min_mapped_i:
 				min_mapped_i = mapped_i
@@ -114,15 +115,22 @@ def wrapImage(sourceImage , h):
 	shiftHeight = - min_mapped_i;
 	shiftWidth = - min_mapped_j;
 
+	print('Bounds ' + str(min_mapped_i) + ' ' + str(max_mapped_i) + ' ' + str(min_mapped_j) + ' ' + str(max_mapped_j));
+	print('shiftHeight ' + str(shiftHeight));
+	print('shiftWidth ' + str(shiftWidth));
+
+
 	destinationImage = np.zeros((newHeight,newWidth,3), dtype=np.uint8);
+
+	print('Destination image size ' + str(destinationImage.shape))
 
 	# write to the new image!
 
 	for i in range(0,height):
 		for j in range(0, width):
-			mappedPos = transform(np.array([[i],[j],[1]]), h);
-			mapped_i = int(mappedPos[0][0])
-			mapped_j = int(mappedPos[1][0])
+			mappedPos = transform(np.array([[j],[i],[1]]), h);
+			mapped_j = int(mappedPos[0][0])
+			mapped_i = int(mappedPos[1][0])
 			destinationImage[mapped_i+shiftHeight][mapped_j+shiftWidth] = sourceImage[i][j];
 
 	im = Image.fromarray(destinationImage)
@@ -134,14 +142,39 @@ def wrapImage(sourceImage , h):
 			# may be done in more neat way!
 			if int(destinationImage[i][j][0]) == 0 and int(destinationImage[i][j][1]) == 0 and int(destinationImage[i][j][2]) == 0:
 				# it's black let's get back to it's inverse!
-				inv_mapped_pos = InvTransform(np.array([[(i - shiftHeight)],[(j - shiftWidth)],[1]]), h)
-				inv_mapped_i = inv_mapped_pos[0][0]
-				inv_mapped_j = inv_mapped_pos[1][0]
+				inv_mapped_pos = InvTransform(np.array([[(j - shiftWidth)], [(i - shiftHeight)],[1]]), h)
+				inv_mapped_j = inv_mapped_pos[0][0]
+				inv_mapped_i = inv_mapped_pos[1][0]
 				if int(inv_mapped_i) < height and  int(inv_mapped_i) > -1 and  int(inv_mapped_j) < width and  int(inv_mapped_j) > -1:
 					destinationImage[i][j] = sourceImage[int(inv_mapped_i)][int(inv_mapped_j)]
 
 	im = Image.fromarray(destinationImage)
 	im.save("without_holes.jpg")
+
+
+
+	# merge original image!
+	ref_image_height = refImage.shape[0]
+	ref_image_width = refImage.shape[1]
+
+	print('Ref-image size ' + str(refImage.shape))
+	mergedImage = np.zeros((newHeight + shiftHeight, newWidth + shiftWidth, 3), dtype=np.uint8);
+
+	# sketch the reference image
+	for i in range(0,ref_image_height):
+		for j in range(0, ref_image_width):
+#			if not( i+ shiftHeight < newHeight and j + shiftWidth < newWidth):
+			mergedImage[i + shiftHeight][j + shiftWidth] = refImage[i][j]
+
+	# sketch the destination image
+	for i in range(0,newHeight):
+		for j in range(0, newWidth):
+			if not( int(destinationImage[i][j][0]) == 0 and int(destinationImage[i][j][0]) == 0 and int(destinationImage[i][j][2]) == 0 ):
+				mergedImage[i][j] = destinationImage[i][j]
+
+	im = Image.fromarray(mergedImage)
+	im.save("after_add_ref_image.jpg")
+
 	return destinationImage;
 
 def main():
@@ -156,7 +189,7 @@ def main():
 #	print(pts);
 	h = calcH(pts);
 #	print('H :' + str(h));
-	print(imageA);
+#	print(imageA);
 	#wrapping
-	wrapImage(imageA, h);
+	wrapAndMergeImage(imageA, h, imageB);
 main();
--- src/get_correspondence.py
+++ src/get_correspondence.py
@@ -1,23 +1,27 @@
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg
 
 # Manually identifies corresponding points from two views
 def getCorrespondence(imageA, imageB):
-	# Display images, select matching points
-	fig = plt.figure()
-	figA = fig.add_subplot(1,2,1)
-	figB = fig.add_subplot(1,2,2)
-	# Display the image
-	# lower use to flip the image
-	figA.imshow(imageA)#,origin='lower')
-	figB.imshow(imageB)#,origin='lower')
-	plt.axis('image')
-	# n = number of points to read
+	getAuto = int(1);
 	num = 20;
-	pts = plt.ginput(n=num, timeout=0)
-#	print(pts);
+	if(getAuto == 0):
+		# Display images, select matching points
+		fig = plt.figure()
+		figA = fig.add_subplot(1,2,1)
+		figB = fig.add_subplot(1,2,2)
+		# Display the image
+		# lower use to flip the image
+		figA.imshow(imageA)#,origin='lower')
+		figB.imshow(imageB)#,origin='lower')
+		plt.axis('image')
+		# n = number of points to read
+		pts = plt.ginput(n=num, timeout=0)
+		print(pts);
+		pts = np.reshape(pts, (2, num/2, 2))
+		return pts
+	pts = np.array([(290.99673461310101, 168.49247971502086), (351.77211557490591, 132.47743914506236), (381.03433603799715, 139.23025925192962), (455.31535721353646, 161.7396596081536), (471.07193746289317, 148.23401939441919), (511.58885810409652, 175.24529982188801), (507.08697803285168, 359.82238274292507), (403.54373639422113, 319.30546210172179), (390.03809618048672, 341.81486245794582), (290.99673461310101, 359.82238274292507), (5.9867999208391893, 139.23025925192951), (80.267821096378498, 109.96803878883827), (107.27910152384732, 121.22273896695026), (177.05824262814178, 161.73965960815349), (195.06576291312103, 148.23401939441908), (224.32798337621227, 175.2452998218879), (208.57140312685544, 353.06956263605764), (98.275341381357748, 303.54888185236484), (82.518761132000918, 326.05828220858882), (3.7358598852168825, 353.06956263605764)])
 	pts = np.reshape(pts, (2, num/2, 2))
 	return pts
 
